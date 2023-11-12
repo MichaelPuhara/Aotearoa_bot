e60890e4cf382fb0cc1cda6c8aea5ff9b9d2ecc4
MichaelPuhara <121073103+MichaelPuhara@users.noreply.github.com>
2023-11-12 19:23:27 +1300

    total rewrite app.py

--- app.py
+++ app.py
@@ -3,41 +3,39 @@ import openai
 import streamlit as st
 from streamlit_chat import message
 
-api_key = os.environ["OPENAI_API_KEY"]
-#openai.api_key = ""
+# Set your OpenAI GPT-3 API key
+api_key = os.environ.get("OPENAI_API_KEY")
+if not api_key:
+    st.warning("OpenAI API key not found. Please set it as an environment variable.")
+    st.stop()
 
-#headers = {
-    #"authorization": st.secrets["OPENAI_API_KEY"],
-    #}
+openai.api_key = api_key
 
-#openai.api_key = ""
+# Define the ChatOpenAI class (assuming it is defined somewhere in your code)
+class ChatOpenAI:
+    def __init__(self, model, prompt, temperature, max_tokens, top_p, frequency_penalty, presence_penalty, stop):
+        # Define the initialization logic for ChatOpenAI
+        pass
 
-#openai.api_key = os.getenv("OPENAI_API_KEY")
-# openAI code
-
+# Your openai_create function
 def openai_create(prompt):
-
-   chat = ChatOpenAI(
-    model="gpt-3.5-turbo-16k",
-    prompt=prompt,
-    temperature=0.9,
-    max_tokens=110,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0.6,
-    stop=[" Human:", " AI:"]
+    chat = ChatOpenAI(
+        model="gpt-3.5-turbo-16k",
+        prompt=prompt,
+        temperature=0.9,
+        max_tokens=110,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0.6,
+        stop=[" Human:", " AI:"]
     )
+    # Perform any necessary processing using the chat instance
+    # ...
 
-    #return response.choices[0].text
-
-
+# Your chatgpt_clone function
 def chatgpt_clone(input, history):
-    history = history or []
-    s = list(sum(history, ()))
-    print(s)
-    s.append(input)
-    inp = ' '.join(s)
-    output = openai_create(inp)
+    # Assuming the logic for openai_create is correct, use it here
+    output = openai_create(input)
     history.append((input, output))
     return history, history
 
@@ -57,23 +55,18 @@ if 'generated' not in st.session_state:
 if 'past' not in st.session_state:
     st.session_state['past'] = []
 
-
 def get_text():
     input_text = st.text_input("Pātai mai: ", key="input")
-    return input_text 
-
+    return input_text
 
 user_input = get_text()
 
-
 if user_input:
     output = chatgpt_clone(user_input, history_input)
-   # history_input.append([user_input, output])
     st.session_state.past.append(user_input)
     st.session_state.generated.append(output[0])
 
 if st.session_state['generated']:
-
     for i in range(len(st.session_state['generated'])-1, -1, -1):
         message(st.session_state["generated"][i], key=str(i))
         message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
